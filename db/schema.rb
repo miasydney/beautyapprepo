b2ad3f1ef9efdbb5565cd8b6308c009a3f210efd
work <work@Mias-MBP.modem>
2022-06-29 14:20:12 +1000

    ran rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_035611) do
+ActiveRecord::Schema.define(version: 2022_06_29_040915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,24 @@ ActiveRecord::Schema.define(version: 2022_06_29_035611) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.integer "price"
+    t.string "business"
+    t.string "instagram"
+    t.boolean "booked", default: false
+    t.date "date"
+    t.time "time"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "city_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_listings_on_category_id"
+    t.index ["city_id"], name: "index_listings_on_city_id"
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,4 +99,7 @@ ActiveRecord::Schema.define(version: 2022_06_29_035611) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "listings", "categories"
+  add_foreign_key "listings", "cities"
+  add_foreign_key "listings", "users"
 end
