3012362b489c32b49dd9b9e96b74a7882aac5171
work <work@Mias-MBP.modem>
2022-07-02 15:23:20 +1000

    began implementation of bought orders page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
 class OrdersController < ApplicationController
   def success
   end
-
+  
   def bought
+    @booked_appointments = current_user.bought_orders
   end
 
   def sold
+    @sold_appointments = current_user.sold_orders
   end
 end
